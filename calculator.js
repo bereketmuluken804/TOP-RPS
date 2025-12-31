ac6532cec8d960d18b8f838285569b447b42842e
M. Bereket <bmuluken804@gmail.com>
2025-12-31 23:19:40 +0300

    Create logic of the game for 1 round

--- calculator.js
+++ calculator.js
@@ -14,3 +14,27 @@ function getHumanChoice(){
     choice = prompt("(Rock, Paper, Scissors)?: ")
     return choice.charAt(0).toUpperCase() + choice.slice(1);
 }
+
+let humanScore = 0;
+let CompScore = 0;
+
+function playRound() {
+
+    let hc = getHumanChoice();
+    let cc = getComputerChoice();
+    let win = "lost!";
+    if(hc === cc) {
+        alert(`It's a Draw\nYou: ${humanScore}           computer: ${CompScore}\n`)
+        return;}
+    
+    else if ((hc === "Rock" && cc === "Scissors") || (hc === "Scissors" && cc === "Paper") || (hc === "Paper" && cc === "Rock")){
+        win ="won!";
+        humanScore++;
+    }
+    else {
+        CompScore++;
+    }
+    alert(`You: ${humanScore}           computer: ${CompScore}\nYou've ${win}\nYou: ${hc}    Computer: ${cc}`)
+
+}
+
